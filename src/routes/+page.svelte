013c77e2614d9289c03215c77832211997dfca37
Hunter Bourgeois <bahunter@gmail.com>
2022-11-10 22:18:57 -0600

    linked to pocketbase successful

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script>
+  import PocketBase from 'pocketbase';
+
+  const client = new PocketBase('http://127.0.0.1:8090');
+
+  const resultList = client.records.getList('orders');
+
+  // const record = client.records.getOne('orders', '9embhu0ditr1998', {
+  //   expand: 'some_relation'
+  // });
+
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+{#await resultList}
+  <p>...Loading</p>
+{:then value}
+  <p>{value.items[0].id}</p>
+{:catch error}
+  <p>{Error.message}</p>
+{/await}
