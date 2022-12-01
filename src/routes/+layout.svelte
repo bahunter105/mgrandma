ebce15fadc86491ec5c7e4425d1debd62bf366af
Hunter Bourgeois <bahunter@gmail.com>
2022-12-01 13:39:11 -0600

    login

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<script>
+  // import PocketBase from 'pocketbase';
+
+  // const pb = new PocketBase('http://127.0.0.1:8090');
+  export let data;
+	const { pb } = data;
+  console.log(pb.authStore)
+  console.log(pb.authData)
+
+  let state ={
+    email:"",
+    password:"",
+    name:"",
+  }
+  let loggedIn = false
+    //FOR LOGIN NOW
+  async function btnLogin(){
+    const authData = await pb.users.authViaEmail(state.email, state.password);
+    if(authData){
+      // authStoreObject = authData;
+      console.log(pb.authStore)
+      loggedIn = true
+      await pb.authStore.exportToCookie(options = {}, key='pb_auth')
+    }
+  }
+  async function btnLogout(){
+    pb.authStore.clear()
+    console.log(pb.authStore)
+    loggedIn = false
+  }
+
+</script>
+
+<nav>
+  <a href="/">home</a>
+  <a href="/login">login page</a>
+  <a href="/register">registration page</a>
+</nav>
+
+
+{#if loggedIn === false }
+  <h2>Login</h2>
+  email: <input type="text" bind:value={state.email} name="">
+  <br/>
+  password: <input type="password" bind:value={state.password} name="">
+  <br/>
+  <button on:click={btnLogin}>login now</button>
+<!-- <p>Loading...</p> -->
+{:else}
+  <button on:click={btnLogout}>log out</button>
+{/if}
+
+<main>
+  <slot />
+</main>
+
+<style>
+	nav {
+		display: flex;
+	}
+	nav a {
+		margin-right: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  import PocketBase from 'pocketbase';
+  // import PocketBase from 'pocketbase';
 
-  const client = new PocketBase('http://127.0.0.1:8090');
+  // const client = new PocketBase('http://127.0.0.1:8090');
 
-  const resultList = client.records.getList('orders');
+  // const resultList = client.records.getList('orders');
 
   // const record = client.records.getOne('orders', '9embhu0ditr1998', {
   //   expand: 'some_relation'
@@ -12,14 +12,11 @@
 </script>
 
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-
-{#await resultList}
+<h1>Welcome to SvelteKit Home Page</h1>
+<!-- {#await resultList}
   <p>...Loading</p>
 {:then value}
   <p>{value.items[0].id}</p>
 {:catch error}
   <p>{Error.message}</p>
-{/await}
+{/await} -->
